95eff0f577e70fe4ac9756ff48867ae66f5a7b20
Victor Cruz <vicdan215@gmail.com>
2021-07-11 01:55:37 -0500

    Implement Icon

---
+++ components/icon.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { icons } from '../constants';
+
+const ICON_SIZES = {
+  small: 'h-4 w-4',
+  regular: 'h-6 w-6',
+  big: 'h-9 w-9',
+};
+
+const colors = {
+  default: {
+    background: 'bg-transparent',
+    backgroundHover: 'bg-gray-300',
+    textColor: 'text-gray-500',
+  },
+  primary: {
+    background: 'bg-primary-100',
+    backgroundHover: 'bg-primary-300',
+    textColor: 'text-primary-500',
+  },
+  secondary: {
+    background: 'bg-secondary-100',
+    backgroundHover: 'bg-secondary-300',
+    textColor: 'text-secondary-500',
+  },
+  tertiary: {
+    background: 'bg-tertiary-100',
+    backgroundHover: 'bg-tertiary-300',
+    textColor: 'text-tertiary-500',
+  },
+};
+
+function getIcon(type = '', textColor = '', size = '') {
+  return <div className={`${size} ${textColor}`}>{icons[type]}</div>;
+}
+
+const Icon = ({
+  color = 'default',
+  type = 'basic',
+  size = 'regular',
+  disablePadding = false,
+  disableBackground = false,
+}) => {
+  const colorInfo = colors[color];
+  const paddingSizes = { small: 'p-1', regular: 'p-2', big: 'p-3' };
+  const background = !disableBackground
+    ? `hover:${colorInfo.backgroundHover} ${colorInfo.background}`
+    : '';
+  return (
+    <div
+      className={`${
+        !disablePadding && paddingSizes[size]
+      } rounded-lg w-min ${background}`}
+    >
+      {getIcon(type, colorInfo.textColor, ICON_SIZES[size])}
+    </div>
+  );
+};
+
+Icon.propTypes = {
+  color: PropTypes.oneOf(['default', 'primary', 'secondary', 'tertiary']),
+  type: PropTypes.oneOf(['basic', 'folder', 'photo', 'plus', 'search'])
+    .isRequired,
+  size: PropTypes.oneOf(['regular', 'small', 'big']),
+  disablePadding: PropTypes.bool,
+  disableBackground: PropTypes.bool,
+};
+
+export default Icon;
---
+++ constants.js
@@ -0,0 +1,68 @@
+export const icons = {
+  basic: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z"
+        clipRule="evenodd"
+      />
+    </svg>
+  ),
+  folder: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
+        clipRule="evenodd"
+      />
+      <path d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
+    </svg>
+  ),
+  photo: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+        clipRule="evenodd"
+      />
+    </svg>
+  ),
+  plus: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+        clipRule="evenodd"
+      />
+    </svg>
+  ),
+  search: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+        clipRule="evenodd"
+      />
+    </svg>
+  ),
+};
