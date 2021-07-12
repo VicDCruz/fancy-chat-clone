148925cc37d45fbc16a0c57ce96e588f516b0a42
Victor Cruz <vicdan215@gmail.com>
2021-07-12 14:39:38 -0500

    Add searchbar

--- components/chats.js
+++ components/chats.js
@@ -3,15 +3,27 @@ import PropTypes from 'prop-types';
 import Icon from './icon';
 import User from './user';
 import Divider from './divider';
+import SearchBar from './searchBar';
+import ListChats from './chats/listChats';
 
 const Chats = props => (
   <div className="flex flex-col gap-3">
-    <div className="flex items-center gap-3">
-      <Icon size="small" type="left" />
-      <h2>Chat</h2>
+    <div className="flex flex-col gap-3 sticky top-0 z-10 bg-white pb-2">
+      <div className="flex items-center gap-3 py-4">
+        <Icon size="small" type="left" />
+        <h2>Chat</h2>
+      </div>
+      <Divider />
+      <User name="Víctor Cruz" size="big" orientation="vertical" />
+      <SearchBar onSearch={text => console.log(text)} />
     </div>
-    <Divider />
-    <User name="Víctor Cruz" orientation="vertical" />
+    <ListChats
+      chats={Array.from(Array(13), (_, index) => index + 1).map(element => ({
+        username: element,
+        imageUrl: '', // Math.random() > 0.5 ? 'https://picsum.photos/200' : '',
+        description: `Descripción - ${element}`,
+      }))}
+    />
   </div>
 );
 
