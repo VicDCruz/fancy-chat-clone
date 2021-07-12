4b7e46c34649c115fcd35a98bc5a68d9771d4c1e
Victor Cruz <vicdan215@gmail.com>
2021-07-12 18:20:04 -0500

    Add timestamp

--- components/messages/bubble.js
+++ components/messages/bubble.js
@@ -5,14 +5,23 @@ const DIRECTIONS = ['right', 'left'];
 
 function Bubble({ message = '', orientation = DIRECTIONS[0] }) {
   return (
-    <div
-      className={`rounded-lg px-2 py-3 max-w-max ${
-        orientation === DIRECTIONS[0]
-          ? 'rounded-br-none bg-secondary-300 shadow-inner self-end ml-1'
-          : 'rounded-bl-none bg-white shadow-md mr-5'
-      }`}
-    >
-      {message}
+    <div className="flex flex-col gap-0.5">
+      <p
+        className={`caption text-gray-400 ${
+          orientation === DIRECTIONS[0] ? 'self-end ml-1' : 'mr-5'
+        }`}
+      >
+        11:26 AM
+      </p>
+      <div
+        className={`rounded-lg px-2 py-3 max-w-max ${
+          orientation === DIRECTIONS[0]
+            ? 'rounded-br-none bg-secondary-300 shadow-inner self-end ml-1'
+            : 'rounded-bl-none bg-white shadow-md mr-5'
+        }`}
+      >
+        {message}
+      </div>
     </div>
   );
 }
