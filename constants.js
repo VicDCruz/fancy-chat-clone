2af9337b2a13ff7cac410a2f8ee9861952ec6069
Victor Cruz <vicdan215@gmail.com>
2021-07-11 22:01:09 -0500

    Add styles & SVGs

--- constants.js
+++ constants.js
@@ -65,4 +65,43 @@ export const icons = {
       />
     </svg>
   ),
+  user: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+        clipRule="evenodd"
+      />
+    </svg>
+  ),
+  left: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+  ),
+  right: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+  ),
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
@@ -5,41 +7,42 @@ module.exports = {
     fontFamily: {
       sans: ['Poppins', 'Arial', 'sans-serif'],
     },
-    extend: {
-      colors: {
-        primary: {
-          100: '#C8FADF',
-          200: '#93F5CA',
-          300: '#5BE3B4',
-          400: '#32C8A2',
-          500: '#00A48A',
-          600: '#008D83',
-          700: '#007276',
-          800: '#00535F',
-          900: '#003D4E',
-        },
-        secondary: {
-          100: '#DEE3FD',
-          200: '#BFC8FB',
-          300: '#9CA9F5',
-          400: '#818EEB',
-          500: '#5968DE',
-          600: '#414DBE',
-          700: '#2C379F',
-          800: '#1C2480',
-          900: '#11166A',
-        },
-        tertiary: {
-          100: '#FCF8E8',
-          200: '#F9F1D2',
-          300: '#EDE1B6',
-          400: '#DCCC9C',
-          500: '#C5B079',
-          600: '#A99058',
-          700: '#8D723C',
-          800: '#725626',
-          900: '#5E4217',
-        },
+    colors: {
+      black: '#202526',
+      gray: colors.gray,
+      red: colors.red,
+      primary: {
+        100: '#C8FADF',
+        200: '#93F5CA',
+        300: '#5BE3B4',
+        400: '#32C8A2',
+        500: '#00A48A',
+        600: '#008D83',
+        700: '#007276',
+        800: '#00535F',
+        900: '#003D4E',
+      },
+      secondary: {
+        100: '#DEE3FD',
+        200: '#BFC8FB',
+        300: '#9CA9F5',
+        400: '#818EEB',
+        500: '#5968DE',
+        600: '#414DBE',
+        700: '#2C379F',
+        800: '#1C2480',
+        900: '#11166A',
+      },
+      tertiary: {
+        100: '#FCF8E8',
+        200: '#F9F1D2',
+        300: '#EDE1B6',
+        400: '#DCCC9C',
+        500: '#C5B079',
+        600: '#A99058',
+        700: '#8D723C',
+        800: '#725626',
+        900: '#5E4217',
       },
     },
   },
