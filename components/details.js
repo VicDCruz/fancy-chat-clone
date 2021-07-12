d23713054ee4d3a908517433f0dfaa1be8e12cdf
Victor Cruz <vicdan215@gmail.com>
2021-07-12 14:39:51 -0500

    Change styles

--- components/details.js
+++ components/details.js
@@ -5,7 +5,7 @@ import Divider from './divider';
 
 const Details = () => (
   <div className="flex flex-col gap-3">
-    <div className="flex items-center gap-3">
+    <div className="flex items-center gap-3 py-4">
       <Icon size="small" type="right" />
       <h2>Shared files</h2>
     </div>
@@ -14,6 +14,7 @@ const Details = () => (
       name="Real estate deals"
       description="10 members"
       orientation="vertical"
+      size="big"
     />
   </div>
 );
