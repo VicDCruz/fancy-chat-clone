eef2e0b17ca1d538249619489c6cd55a9888ff07
Victor Cruz <vicdan215@gmail.com>
2021-07-12 14:40:27 -0500

    Change size values

--- components/user.js
+++ components/user.js
@@ -4,9 +4,9 @@ import Image from 'next/image';
 import Icon from './icon';
 
 const IMAGE_SIZES = {
-  small: { width: 40, height: 40 },
-  regular: { width: 70, height: 70 },
-  big: { width: 100, height: 100 },
+  small: { width: 25, height: 25 },
+  regular: { width: 43, height: 43 },
+  big: { width: 65, height: 65 },
 };
 
 const ORIENTATIONS = ['horizontal', 'vertical'];
@@ -15,11 +15,11 @@ function User({
   name = '',
   imageUrl = '',
   orientation = ORIENTATIONS[0],
-  imageSize = 'regular',
+  size = 'regular',
   description = '',
 }) {
   const direction = orientation === ORIENTATIONS[0] ? 'flex-row' : 'flex-col';
-  const size = IMAGE_SIZES[imageSize];
+  const imageSize = IMAGE_SIZES[size];
   return (
     <div className={`flex ${direction} gap-4 items-center`}>
       {imageUrl ? (
@@ -28,17 +28,21 @@ function User({
             className="rounded-full"
             src={imageUrl}
             alt="Image profile"
-            width={size.width}
-            height={size.height}
+            width={imageSize.width}
+            height={imageSize.height}
           />
         </div>
       ) : (
-        <div className="border rounded-full w-min">
-          <Icon type="user" size={imageSize} disableBackground />
+        <div className="border border-primary-300 bg-primary-100 rounded-full w-min">
+          <Icon type="user" color="primary" size={size} disableBackground />
         </div>
       )}
-      <div className={`flex ${direction} gap-1 text-center`}>
-        <h2>{name}</h2>
+      <div
+        className={`flex flex-col gap-1 ${
+          orientation === ORIENTATIONS[1] ? 'text-center' : ''
+        }`}
+      >
+        {orientation === ORIENTATIONS[0] ? <p>{name}</p> : <h2>{name}</h2>}
         {description && <p className="text-gray-400 caption">{description}</p>}
       </div>
     </div>
@@ -49,7 +53,7 @@ User.propTypes = {
   name: PropTypes.string.isRequired,
   imageUrl: PropTypes.string,
   orientation: PropTypes.oneOf(ORIENTATIONS),
-  imageSize: PropTypes.oneOf(Object.keys(IMAGE_SIZES)),
+  size: PropTypes.oneOf(Object.keys(IMAGE_SIZES)),
   description: PropTypes.string,
 };
 
